ff7ab0153abc0d1a33b312b8707f628384a0a7bf
Torin Rozzelle <torinadamrozzelle@gmail.com>
2024-12-23 09:16:56 -0500

    Checkpoint commit

--- src/donor_db_builder/ConfigScreen.py
+++ src/donor_db_builder/ConfigScreen.py
@@ -1,6 +1,8 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Input, Label, Tree
-from textual.containers import Container
+from textual.widgets import Header, Footer, Static, Input, Label, Tree, Select
+from textual.containers import Container, ScrollableContainer, Vertical
+from textual.widget import Widget
+from config import Settings, Config
 
 # class LabeledInput(Static):
 #
@@ -14,38 +16,89 @@ from textual.containers import Container
 #         yield Label(self.label, id=f"{self.id}-cf-label")
 #         yield Input(placeholder=self.placeholder, id=f"{self.id}-cf-input")
 
-class LabeledInput(Static):
 
-    def __init__(self, label: str, placeholder: str, id: str):
+class LabeledInput(Widget):
+    def __init__(self, label: str, placeholder: str, value: str = None):
         self.label = label
         self.placeholder = placeholder
+        self.value = value
         super().__init__()
 
     def compose(self) -> ComposeResult:
-        yield Label(self.label, id=f"cf-label")
-        yield Input(placeholder=self.placeholder, id=f"cf-input")
+        yield Label(self.label)
+        yield Input(placeholder=self.placeholder)
+
 
 class ConfigScreenBody(Static):
-
     def compose(self) -> ComposeResult:
-        yield LabeledInput("DB Host", placeholder="127.0.0.1", id="host")
-        yield LabeledInput("DB Pass", placeholder="****", id="pass")
+        yield LabeledInput("DB Host", placeholder="127.0.0.1")
+        yield LabeledInput("DB Pass", placeholder="****")
 
         # yield Container(LabeledInput("DB Pass", placeholder="****"))
 
-class ConfigScreenMain(Static):
 
+class ConfigScreenMain(Static):
     def compose(self) -> ComposeResult:
         yield Container(ConfigScreenBody())
 
-class ConfigScreen(App):
 
+class ConfigScreen(App):
     CSS_PATH = "ConfigScreen.tcss"
 
+    def __init__(self):
+        self.config = Settings()
+        super().__init__()
+
+    # def on_mount(self) -> None:
+    # footer = self.query_one("#modal_footer", Static)
+    # footer.display = False
+    # if self.error_message:
+    #     footer.update(self.error_message.output())
+    #     footer.display = True
+
+    # self.query_one("#host", Input).border_title = "Host"
+
+    def on_mount(self) -> None:
+        self.query_one("#host", Input).border_title = "Host"
+        self.query_one("#user", Input).border_title = "User"
+        self.query_one("#password", Input).border_title = "Password"
+
     def compose(self) -> ComposeResult:
+        # yield Header()
+        settings, labels = self.config.get_all_with_labels()
+        # for field_name, value in settings.items():
+        #     yield ScrollableContainer(
+        #         LabeledInput(
+        #             label=labels[field_name]["label"],
+        #             placeholder=labels[field_name]["placeholder"],
+        #             value=str(value) if not None else "",
+        #         )
+        #     )
+        # yield Footer()
+
         yield Header()
-        yield Container(ConfigScreenMain())
+        with Vertical():
+            with Vertical(classes="main_container"):
+                yield Label("Connect to a Host")
+
+                yield Input(value="localhost", id="host", placeholder="Host:Port")
+                yield Input(value="", id="user", placeholder="Username")
+                yield Input(
+                    value="", id="password", placeholder="Password", password=True
+                )
+                # yield Select(
+                #     options=[],
+                #     id="credential_profile",
+                #     value="",
+                #     prompt="Select a credential profile (optional)",
+                # )
+
+                # yield AutoComplete(
+                #     Input(value=self.host, id="host", placeholder="Host:Port"),
+                #     Dropdown(id="dropdown_items", items=self.options_available_hosts),
+                # )
         yield Footer()
+
 
 if __name__ == "__main__":
     app = ConfigScreen()
--- src/donor_db_builder/config.py
+++ src/donor_db_builder/config.py
@@ -1,50 +1,161 @@
 import os
 import json
 import click
+from pathlib import Path
+from dotenv import dotenv_values
+from tomlkit.toml_document import TOMLDocument
+from tomlkit.toml_file import TOMLFile
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional, Any
 
-# CONFIG_DIR = click.get_app_dir("donor-db-builder")
-CONFIG_DIR = os.getcwd() + "/.config"
-CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
+CONFIG_DIR = Path(__file__).resolve().parent / "config"
+# CONFIG_FILE = "config.json"
+ENV_FILE = CONFIG_DIR / ".env"
+
+config_labels = {
+    "DB_HOST": {
+        "label": "DB Host",
+        "description": "The host of the database",
+        "placeholder": "localhost",
+    },
+    "DB_PORT": {
+        "label": "DB Port",
+        "description": "The port of the database",
+        "placeholder": "5432",
+    },
+    "DB_USER": {
+        "label": "DB User",
+        "description": "The user of the database",
+        "placeholder": "postgres",
+    },
+    "DB_PASS": {
+        "label": "DB Password",
+        "description": "The password of the database",
+        "placeholder": "****",
+    },
+    "SOCRATA_API_KEY": {
+        "label": "Socrata Key",
+        "description": "The API key for the Socrata API",
+        "placeholder": "****",
+    },
+}
 
 
-class Config(object):
+class Config(BaseSettings):
+    DB_HOST: str = Field(default="localhost", description="The host of the database")
+    DB_PORT: int = Field(default=5432, description="The port of the database")
+    DB_USER: str = Field(default="postgres", description="The user of the database")
+    DB_PASS: str = Field(
+        default="<PASSWORD>", description="The password of the database"
+    )
+    SOCRATA_API_KEY: Optional[str] = Field(
+        default=None, description="The API key for the Socrata API"
+    )
 
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+    # DB_NAME: str = "donor_atlas"
+    # DB_SCHEMA: str = "public"
+    # DB_DRIVER: str = "psycopg2"
+    # DB_URL: str = Field(
+    #     None,
+    # )
+
+
+class Settings:
     def __init__(self):
-        self.config = self.load_config()
+        self.config = Config()
+        self.labels = config_labels
 
-    def load_config(self):
-        """Load configuration from file"""
-        if not os.path.exists(CONFIG_FILE):
-            return {}
+    def save_to_env(self) -> None:
+        """Save configuration to .env file"""
+        env_content = []
+        for field, value in self.config.model_dump().items():
+            env_content.append(f"{field}={value}")
+        with open(ENV_FILE, "w") as f:
+            f.write("\n".join(env_content))
 
-        try:
-            with open(CONFIG_FILE, 'r') as f:
-                return json.load(f)
-        except Exception as e:
-            click.echo(f"Error loading config: {e}", err=True)
-            return {}
-
-
-    def save_config(self, config):
-        """Save configuration to file"""
-        if not os.path.exists(CONFIG_DIR):
-            os.makedirs(CONFIG_DIR)
-
-        try:
-            with open(CONFIG_FILE, 'w') as f:
-                json.dump(config, f, indent=2)
-        except Exception as e:
-            click.echo(f"Error saving config: {e}", err=True)
-
-
-    def get_setting(self, key, default=None):
+    def get(self, field_name: str) -> Any:
         """Get a setting from config"""
-        config = self.load_config()
-        return config.get(key, default)
+        if not hasattr(self.config, field_name):
+            raise ValueError(f"Field {field_name} not found in config")
+        return getattr(self.config, field_name)
 
-
-    def set_setting(self, key, value):
+    def set(self, field_name: str, value: Any) -> None:
         """Set a setting in config"""
-        config = self.load_config()
-        config[key] = value
-        self.save_config(config)
+        if not hasattr(self.config, field_name):
+            raise ValueError(f"Field {field_name} not found in config")
+        setattr(self.config, field_name, value)
+        self.save_to_env()
+
+    def get_all(self):
+        return self.config.model_dump()
+
+    def get_all_with_labels(self):
+        return (self.config.model_dump(), self.labels)
+
+    def load_from_env(self) -> None:
+        """Load configuration from .env file"""
+        env_vars = dotenv_values(ENV_FILE)
+        for field, value in env_vars.items():
+            self.set(field, value)
+
+    def update_from_dict(self, config_dict: dict) -> None:
+        """Update configuration from a dictionary"""
+
+        validated_config = self.config.model_validate(config_dict)
+
+        for field, value in config_dict.items():
+            self.set(field, value)
+        self.save_to_env()
+
+
+# class Config(object):
+#     """
+#     Class that handles loading and saving configuration to file
+#     """
+#
+#     def __init__(self):
+#         self.config = self.load()
+#
+#     def load(self):
+#         """Load configuration from file"""
+#         if not os.path.exists(CONFIG_DIR):
+#             return {}
+#
+#         try:
+#             with open(CONFIG_DIR / CONFIG_FILE, "r") as f:
+#                 return json.load(f)
+#         except Exception as e:
+#             click.echo(f"Error loading config: {e}", err=True)
+#             return {}
+#
+#     def save(self):
+#         """Save configuration to file"""
+#         if not os.path.exists(CONFIG_DIR):
+#             os.makedirs(CONFIG_DIR)
+#
+#         try:
+#             with open(CONFIG_DIR / CONFIG_FILE, "w") as f:
+#                 json.dump(self.config, f, indent=2)
+#         except Exception as e:
+#             click.echo(f"Error saving config: {e}", err=True)
+#
+#     def get(self, key, default=None):
+#         """Get a setting from config"""
+#         return self.config.get(key, default)
+#
+#     def set(self, key, value):
+#         """Set a setting in config"""
+#         self.config[key] = value
+
+
+if __name__ == "__main__":
+    settings = Settings()
+    settings.save_to_env()
